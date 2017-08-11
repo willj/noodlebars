2ec5ee8574e283f0f9b72482bb876312ec730f6f
Will <willj@users.noreply.github.com>
2017-08-11 14:36:55 +0100

    Add getSlug and getUrlPath with tests

--- src/test/urls.test.js
+++ src/test/urls.test.js
@@ -1,5 +1,6 @@
 const assert = require("assert");
 const urls = require("../urls");
+const settings = require("../settings");
 
 describe("Urls", function(){
 
@@ -47,6 +48,162 @@ describe("Urls", function(){
             let slug = urls.getSlug(testString);
 
             assert.strictEqual(slug, expectedSlug);
+        });
+
+    });
+
+    describe("getUrlPath", function(){
+
+        it("returns the permalink if one is provided in pageAttributes", function(){
+            let pageAttributes = {
+                permalink: "projects/project-1.html"
+            };
+
+            let expectedResult = {
+                path: "projects/",
+                fileName: "project-1.html",
+                permalink: "/projects/project-1.html"
+            };
+
+            let urlPath = urls.getUrlPath("test/files", "project-page.html", pageAttributes);
+
+            assert.deepEqual(urlPath, expectedResult);
+        });
+
+        it("returns the correct path and filename as index.html when the permalink has no extension", function(){
+            let pageAttributes = {
+                permalink: "projects/project-2"
+            };
+
+            let expectedResult = {
+                path: "projects/project-2/",
+                fileName: "index.html",
+                permalink: "/projects/project-2/"
+            };
+
+            let urlPath = urls.getUrlPath("test/files", "project-page.html", pageAttributes);
+
+            assert.deepEqual(urlPath, expectedResult);
+        });
+
+        it("returns a path containing the date for posts when useDateInPostUrls is enabled", function(){
+            let pageAttributes = {
+                type: "post",
+                postDate: new Date(2020, 11, 25),
+                title: "Hello World"
+            };
+
+            settings.addSetting("useDateInPostUrls", true);
+            settings.addSetting("removeFileExtFromUrls", true);
+
+            let expectedResult = {
+                path: "2020/12/hello-world/",
+                fileName: "index.html",
+                permalink: "/2020/12/hello-world/"
+            };
+
+            let urlPath = urls.getUrlPath("test/files", "a-post.html", pageAttributes);
+
+            assert.deepEqual(urlPath, expectedResult);
+        });
+
+        it("returns a path that matches post title when useDateInPostUrls is disabled", function(){
+            let pageAttributes = {
+                type: "post",
+                postDate: new Date(2020, 11, 25),
+                title: "Hello World"
+            };
+
+            settings.addSetting("useDateInPostUrls", false);
+            settings.addSetting("removeFileExtFromUrls", true);
+
+            let expectedResult = {
+                path: "hello-world/",
+                fileName: "index.html",
+                permalink: "/hello-world/"
+            };
+
+            let urlPath = urls.getUrlPath("test/files", "a-post.html", pageAttributes);
+
+            assert.deepEqual(urlPath, expectedResult);
+        });
+
+        it("returns a path with date and a fileName that matches post title when removeFileExtFromUrls is false and useDateInPostUrls is true", function(){
+            let pageAttributes = {
+                type: "post",
+                postDate: new Date(2020, 11, 25),
+                title: "Hello World"
+            };
+
+            settings.addSetting("useDateInPostUrls", true);
+            settings.addSetting("removeFileExtFromUrls", false);
+
+            let expectedResult = {
+                path: "2020/12/",
+                fileName: "hello-world.html",
+                permalink: "/2020/12/hello-world.html"
+            };
+
+            let urlPath = urls.getUrlPath("test/files", "a-post.html", pageAttributes);
+
+            assert.deepEqual(urlPath, expectedResult);
+        });
+
+        it("returns a path with no date and a fileName that matches post title when removeFileExtFromUrls and useDateInPostUrls are false", function(){
+            let pageAttributes = {
+                type: "post",
+                postDate: new Date(2020, 11, 25),
+                title: "Hello World"
+            };
+
+            settings.addSetting("useDateInPostUrls", false);
+            settings.addSetting("removeFileExtFromUrls", false);
+
+            let expectedResult = {
+                path: "",
+                fileName: "hello-world.html",
+                permalink: "/hello-world.html"
+            };
+
+            let urlPath = urls.getUrlPath("test/files", "a-post.html", pageAttributes);
+
+            assert.deepEqual(urlPath, expectedResult);
+        });
+
+        it("returns a path matching the file path with the filename as index.html when no permalink supplied, is not a post, and removeFileExtFromUrls is true", function(){
+            let pageAttributes = {
+                type: "page"
+            };
+
+            settings.addSetting("removeFileExtFromUrls", true);
+
+            let expectedResult = {
+                path: "test/about/",
+                fileName: "index.html",
+                permalink: "/test/about/"
+            };
+
+            let urlPath = urls.getUrlPath("test", "about.html", pageAttributes);
+
+            assert.deepEqual(urlPath, expectedResult);
+        });
+
+        it("returns a path matching the file path and matching filenames when no permalink supplied, is not a post, and removeFileExtFromUrls is false", function(){
+            let pageAttributes = {
+                type: "page"
+            };
+
+            settings.addSetting("removeFileExtFromUrls", false);
+
+            let expectedResult = {
+                path: "test",
+                fileName: "about.html",
+                permalink: "/test/about.html"
+            };
+
+            let urlPath = urls.getUrlPath("test", "about.html", pageAttributes);
+
+            assert.deepEqual(urlPath, expectedResult);
         });
 
     });
--- src/urls.js
+++ src/urls.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const path = require("path");
+const settings = require("./settings");
 
 function getSlug(title){
     return title.replace(/[^a-z0-9]+/ig, " ")
@@ -8,8 +9,69 @@ function getSlug(title){
             .toLowerCase();
 }
 
+function getUrlPath(filePath, fileName, pageAttributes){
+        
+    // if attributes have a permalink defined
+    if ("permalink" in pageAttributes){
+        let plink = path.parse(pageAttributes.permalink);
 
+        if (plink.ext){
+            return {
+                path: plink.dir + "/",
+                fileName: plink.name + plink.ext,
+                permalink: "/" + plink.dir + "/" + plink.name + plink.ext
+            };
+        } else {
+            return {
+                path: plink.dir + "/" + plink.name + "/",
+                fileName: "index.html",
+                permalink: "/" + plink.dir + "/" + plink.name + "/"
+            };
+        }
+    }
+    
+    // if it's a post
+    if (("type" in pageAttributes && pageAttributes.type == "post")){
+        let postPathDate = "";
+
+        if (settings.getSetting("useDateInPostUrls")){
+            postPathDate = pageAttributes.postDate.getFullYear() + "/"; 
+            postPathDate += (pageAttributes.postDate.getMonth() + 1) + "/";
+        }
+
+        if (settings.getSetting("removeFileExtFromUrls")){
+            return {
+                path: postPathDate + getSlug(pageAttributes.title) + "/",
+                fileName: "index.html",
+                permalink: "/" + postPathDate + getSlug(pageAttributes.title) + "/"
+            }
+        } else {
+            return {
+                path: postPathDate,
+                fileName: getSlug(pageAttributes.title) + ".html",
+                permalink: "/" + postPathDate + getSlug(pageAttributes.title) + ".html"
+            }
+        }
+    }
+
+    // no permalink, and isn't a post
+    if (settings.getSetting("removeFileExtFromUrls")){
+        return {
+            path: filePath + "/" + path.parse(fileName).name + "/",
+            fileName: "index.html",
+            permalink: "/" + filePath + "/" + path.parse(fileName).name + "/"
+        }
+    } else {
+        return {
+            path: filePath,
+            fileName: path.parse(fileName).name + ".html",
+            permalink: "/" + filePath + "/" + path.parse(fileName).name + ".html"
+        }
+    }
+
+}
 
 module.exports = {
-    getSlug: getSlug
+    getSlug: getSlug,
+    getUrlPath: getUrlPath
 };
