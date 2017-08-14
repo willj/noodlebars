a5cab79c1d431d8142fb16aacee88af4ee1ed014
Will <willj@users.noreply.github.com>
2017-08-14 11:49:34 +0100

    add files.js

---
+++ src/files.js
@@ -0,0 +1,47 @@
+"use strict";
+
+const path = require("path");
+const fs = require("fs");
+const rimraf = require("rimraf");
+let settings;
+
+function createDir(path) {
+
+    let createdPath = "";
+
+    path.split("/").forEach((dir) => {
+        if (dir == "." || dir == "") {
+            return;
+        }
+        
+        try {
+            fs.mkdirSync(createdPath + dir);
+        } catch (error) { }
+
+        createdPath += dir + "/";
+    });
+}
+
+function removeDir(path) {
+    rimraf.sync(path);
+}
+
+function copyFile(from, to) {
+    fs.createReadStream(from).pipe(fs.createWriteStream(to));
+}
+
+function writeFile(file, data) {
+    fs.writeFile(settings.getSetting("outputDir") + file, data, (err) => {
+        if (err) return console.error(err);
+    });
+}
+
+module.exports = function(appSettings){
+    settings = appSettings;
+    return {
+        copyFile: copyFile,
+        createDir: createDir,
+        removeDir: removeDir,
+        writeFile: writeFile
+    };
+};
